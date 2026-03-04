76c3e8b1a0db76004d9bb9b576c1590010518017
arnicauser01 <arnicauser01@outlook.com>
2026-03-03 19:41:11 -0500

    Update file1.rs

--- file1.rs
+++ file1.rs
@@ -2,3 +2,4 @@ use std::env;
 
 // ruleid: args-os
 let args: Vec<String> = env::args().map(|arg| arg.into_string().unwrap()).collect();
+# Agentic Rule (ARNIE_ACCESS_PERMISSION): Require admin role to for file download
